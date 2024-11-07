85306a23ec62a1647a3c4178eade7637a1b6398f
GabrielMMVI <161396206+GabrielMMVI@users.noreply.github.com>
2024-11-07 00:46:11 -0300

    FilaCircular

---
+++ filaCircular.c
@@ -0,0 +1,70 @@
+// -> Descomentar para rodar
+// #include <stdio.h>
+// #define MAX 5
+//
+// typedef struct {
+//     int itens[MAX];
+//     int inicio, fim;
+// }Fila;
+//
+// // -> Inicializar fila:
+// void inicializaFila(Fila *fila) {
+//     fila->inicio = -1;
+//     fila->fim =-1;
+// }
+//
+// // -> Verificar se a fila está cheia:
+//
+// int filaCheia(Fila *fila) {
+//     return ((fila->fim  + 1) % MAX == fila->inicio);
+// }
+//
+// // -> Veridica se a fila está vazia:
+// int filaVazia(Fila *fila) {
+//     return (fila->inicio == -1);
+// }
+//
+// // -> Enfileirando valores:
+// void enfileirar(Fila *fila, int valor) {
+//     if (filaCheia(fila)) {
+//         printf("Fila cheia!\n");
+//     } else {
+//         if (filaVazia(fila)) {
+//             fila->inicio = 0;
+//         }
+//         fila->fim = (fila->fim + 1) % MAX;
+//         fila->itens[fila->fim] = valor;
+//         printf("Enfileirado: %d\n", valor);
+//     }
+// }
+//
+// // -> Desenfileirando:
+// int desenfileirar(Fila *fila) {
+//     int valor;
+//     if (filaVazia(fila)) {
+//         printf("Fila vazia!\n");
+//         return -1;
+//     } else {
+//         valor = fila->itens[fila->inicio];
+//         if (fila->inicio == fila->fim) {
+//             fila->inicio = fila->fim = -1;
+//         } else {
+//             fila->inicio = (fila->inicio + 1) % MAX;
+//         }
+//         return valor;
+//     }
+// }
+//
+// int main() {
+//     Fila fila;
+//
+//     inicializaFila(&fila);
+//
+//     enfileirar(&fila, 10);
+//     enfileirar(&fila, 20);
+//     enfileirar(&fila, 30);
+//
+//     printf("Desenfileirado: %d\n", desenfileirar(&fila));
+//
+//     enfileirar(&fila, 40);
+// }
--- filaLinear.c
+++ filaLinear.c
@@ -1,71 +1,79 @@
-#include <stdio.h>
-#define MAX 5
-
-typedef struct {
-    int itens[MAX];
-    int inicio, fim;
-} Fila;
-
-// -> inicializando a fila:
-void inicializarFila(Fila *fila) {
-    fila->inicio = -1;
-    fila->fim = -1;
-}
-
-// -> Verificando se a fila está vazia:
-int filaVazia(Fila *fila) {
-    return (fila->inicio == -1);
-}
-
-// -> Verificando se a fila está cheia:
-int filaCheia(Fila *fila) {
-    return (fila->fim == MAX -1);
-}
-
-// -> Enfileirando:
-void enfilar(Fila *fila, int valor) {
-    if (filaCheia(fila)) {
-        printf("Fila cheia!\n");
-    } else {
-        if (fila->inicio == -1) {
-            fila->inicio = 0;
-        }
-        fila->fim++;
-        fila->itens[fila->fim] = valor;
-        printf("Valor enfileirado: %d\n", valor);
-    }
-}
-
-// -> Desenfileirando:
-int desenfileirar(Fila *fila) {
-    int valor;
-    if (filaVazia(fila)) {
-        printf("A fila já está vazia!\n");
-        return -1;
-    } else {
-        valor = fila->itens[fila->inicio];
-        fila->inicio++;
-        if (fila->inicio > fila->fim) {
-            fila->inicio = fila->fim = -1; // -> Reseta a fila.
-        }
-        return valor;
-    }
-}
-
-// -> Consultando o início da fila:
-int consultaInicio(Fila *fila) {
-    if (filaVazia(fila)) {
-        printf("A fila está vazia!\n");
-        return -1;
-    } else {
-        return fila->itens[fila->inicio];
-    }
-}
-
-int main() {
-    Fila fila;
-    inicializarFila(&fila);
-
-    enfilar(&fila, 10);
-    enfilar(&fila, 20);
-}
+// #include <stdio.h>
+// #define MAX 5
+//
+// typedef struct {
+//     int itens[MAX];
+//     int inicio, fim;
+// } Fila;
+//
+// // -> inicializando a fila:
+// void inicializarFila(Fila *fila) {
+//     fila->inicio = -1;
+//     fila->fim = -1;
+// }
+//
+// // -> Verificando se a fila está vazia:
+// int filaVazia(Fila *fila) {
+//     return (fila->inicio == -1);
+// }
+//
+// // -> Verificando se a fila está cheia:
+// int filaCheia(Fila *fila) {
+//     return (fila->fim == MAX -1);
+// }
+//
+// // -> Enfileirando:
+// void enfilar(Fila *fila, int valor) {
+//     if (filaCheia(fila)) {
+//         printf("Fila cheia!\n");
+//     } else {
+//         if (fila->inicio == -1) {
+//             fila->inicio = 0;
+//         }
+//         fila->fim++;
+//         fila->itens[fila->fim] = valor;
+//         printf("Valor enfileirado: %d\n", valor);
+//     }
+// }
+//
+// // -> Desenfileirando:
+// int desenfileirar(Fila *fila) {
+//     int valor;
+//     if (filaVazia(fila)) {
+//         printf("A fila já está vazia!\n");
+//         return -1;
+//     } else {
+//         valor = fila->itens[fila->inicio];
+//         fila->inicio++;
+//         if (fila->inicio > fila->fim) {
+//             fila->inicio = fila->fim = -1; // -> Reseta a fila.
+//         }
+//         return valor;
+//     }
+// }
+//
+// // -> Consultando o início da fila:
+// int consultaInicio(Fila *fila) {
+//     if (filaVazia(fila)) {
+//         printf("A fila está vazia!\n");
+//         return -1;
+//     } else {
+//         return fila->itens[fila->inicio];
+//     }
+// }
+//
+// int main() {
+//     Fila fila;
+//     inicializarFila(&fila);
+//
+//     enfilar(&fila, 10);
+//     enfilar(&fila, 20);
+//     enfilar(&fila, 30);
+//
+//     printf("Início da fila: %d\n", consultaInicio(&fila));
+//
+//     desenfileirar(&fila);
+//     printf("Inicio após desenfileirar: %d\n", consultaInicio(&fila));
+//
+//     return 0;
+// }
